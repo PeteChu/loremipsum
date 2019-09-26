6fd1209cc856a8b10058b8dea7e4bd711178e546
SchecterZa <phuengton.ch.57@ubu.ac.th>
2019-09-26 11:28:25 +0700

    Added copy to clipboard

--- src/components/LoremIpsum.js
+++ src/components/LoremIpsum.js
@@ -3,6 +3,11 @@ import { LoremIpsum as LI } from "lorem-ipsum";
 import "../css/LoremIpsum.css";
 
 class LoremIpsum extends Component {
+  constructor(props) {
+    super(props);
+    this.copyToClipboard = this.copyToClipboard.bind(this);
+  }
+
   loremIpsum(numberOfParagraph) {
     const lorem = new LI(
       {
@@ -18,16 +23,31 @@ class LoremIpsum extends Component {
       "plain",
       "\n"
     );
-    var paragraphs = [...Array(numberOfParagraph).keys()].map(n => (
-      <p className="lorem-text" key={n}>
-        {lorem.generateParagraphs(1)}
-      </p>
-    ));
+    const paragraphs = [...Array(numberOfParagraph).keys()].map(
+      n => "\t" + lorem.generateParagraphs(1) + "\n\n"
+    );
     return paragraphs;
   }
+
+  copyToClipboard() {
+    const copyText = document.getElementById("lorem").textContent;
+    const textArea = document.createElement("textarea");
+    textArea.setAttribute("type", "hidden");
+    textArea.value = copyText;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+  }
+
   render() {
     return (
-      <div className="lorem scroll" id="lorem">
+      <div className="lorem scroll" id="lorem" ref={this.textArea}>
+        <input
+          type="button"
+          value="copy"
+          onClick={this.copyToClipboard}
+          id="copyToClipboard"
+        />
         {this.loremIpsum(this.props.numberOfParagraph)}
       </div>
     );
