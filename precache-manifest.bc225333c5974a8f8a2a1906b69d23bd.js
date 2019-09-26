811d682600c08fd2a3d5101c1943e2197ae80386
SchecterZa <phuengton.ch.57@ubu.ac.th>
2019-09-26 11:32:50 +0700

    Updates

---
+++ precache-manifest.bc225333c5974a8f8a2a1906b69d23bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cba625e3545fe08eefa8fb293ec98eb",
+    "url": "/loremipsum/index.html"
+  },
+  {
+    "revision": "a76a82bbd8380a3dc9dd",
+    "url": "/loremipsum/static/css/main.e024e7b9.chunk.css"
+  },
+  {
+    "revision": "6c38cd836d2cb880b6b8",
+    "url": "/loremipsum/static/js/2.1df690aa.chunk.js"
+  },
+  {
+    "revision": "a76a82bbd8380a3dc9dd",
+    "url": "/loremipsum/static/js/main.773402bf.chunk.js"
+  },
+  {
+    "revision": "b2aaf5e4399c8c947f8b",
+    "url": "/loremipsum/static/js/runtime-main.0ca69a56.js"
+  }
+]);
